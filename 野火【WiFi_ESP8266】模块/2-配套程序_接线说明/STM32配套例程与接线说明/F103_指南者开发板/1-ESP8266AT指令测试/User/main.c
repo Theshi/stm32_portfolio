4bd8bc6adfe1660e7bacbb77f27381bf28bd2e80
Theshi <1663737618@qq.com>
2026-06-22 23:37:01 +0800

    开始搞HTTP访问的状态机

--- 野火【WiFi_ESP8266】模块/2-配套程序_接线说明/STM32配套例程与接线说明/F103_指南者开发板/1-ESP8266AT指令测试/User/main.c
+++ 野火【WiFi_ESP8266】模块/2-配套程序_接线说明/STM32配套例程与接线说明/F103_指南者开发板/1-ESP8266AT指令测试/User/main.c
@@ -57,3 +57,79 @@ int main(void)
   }	
 }	
 /*********************************************END OF FILE**********************/
+/*HTTP状态机*/
+
+typedef enum
+{
+  HTTP_STATE_IDLE ,//空闲状态
+  HTTP_SATEP_CIPSTART,//正在链接TCP服务器
+  HTTP_STATE_WAIT_CONNECT,//等待TCP服务器连接成功(OK/ALREADY CONNECTED)
+  HTTP_STATE_CIPSEND, //正在发送数据
+  HTTP_STATE_WAIT_PROMPT,//等待发送数据的提示符(>)
+  HTTP_STATE_WAIT_SEND_OK,//等待发送数据成功的回复(OK/SEND OK)
+  HTTP_STATE_SEND_HTTP,       // 发送 HTTP GET 请求
+  HTTP_STATE_WAIT_RESPONSE,   // 等待服务器响应数据
+  HTTP_STATE_DONE,            // 完成
+  HTTP_STATE_ERROR            // 出错
+} HTTP_StateTypeDef;
+
+HTTP_StateTypeDef http_state = HTTP_STATE_IDLE;
+uint8_t Http_Time_out =0;//HTTP状态机的超时计数器
+
+/*在main()的主循环中调用状态机*/
+void HTTP_Request_Status_Machine(void)
+{
+  switch(http_state)
+  {
+    case HTTP_STATE_IDLE:
+      // 空闲状态，可以根据需要触发 HTTP 请求
+      break;
+      
+    case HTTP_SATEP_CIPSTART:
+      ESP8266_Cmd("AT+CIPSTART=\"TCP\",\"worldtimeapi.org\",80",
+                        "OK", "ALREADY CONNECTED", 5000);// 发送 AT+CIPSTART 命令连接 TCP 服务器
+      // Usart_SendString(macESP8266_USARTx, 
+      //           "AT+CIPSTART=\"TCP\",\"worldtimeapi.org\",80\r\n");//这边两个发送的发送是不是重复了
+      http_state= HTTP_STATE_WAIT_CONNECT;
+      Http_Time_out = 0;                  
+      break;
+      
+    case HTTP_STATE_WAIT_CONNECT:
+      if(strEsp8266_Fram_Record.InfBit.FramFinishFlag)// 等待服务器连接成功的回复
+      {
+        
+      }
+      break;
+      
+    case HTTP_STATE_CIPSEND:
+      // 发送 AT+CIPSEND 命令准备发送数据
+      break;
+      
+    case HTTP_STATE_WAIT_PROMPT:
+      // 等待发送数据的提示符 (>)
+      break;
+      
+    case HTTP_STATE_WAIT_SEND_OK:
+      // 等待发送数据成功的回复 (OK/SEND OK)
+      break;
+      
+    case HTTP_STATE_SEND_HTTP:
+      // 发送 HTTP GET 请求
+      break;
+      
+    case HTTP_STATE_WAIT_RESPONSE:
+      // 等待服务器响应数据
+      break;
+      
+    case HTTP_STATE_DONE:
+      // 请求完成，可以处理响应数据或重置状态机
+      break;
+      
+    case HTTP_STATE_ERROR:
+      // 处理错误情况，可以重置状态机或记录错误信息
+      break;
+      
+    default:
+      break;
+  }
+}
